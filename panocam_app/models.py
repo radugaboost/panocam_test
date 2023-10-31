2b958be88b6913a9c051c47796b65866635921ae
kirillsev1 <kirillprih47@gmail.com>
2023-10-31 11:38:37 +0300

    added ml model and str view

--- panocam_app/models.py
+++ panocam_app/models.py
@@ -16,6 +16,9 @@ class Configuration(models.Model):
     class Meta:
         db_table = 'configuration'
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Camera(models.Model):
     ip = models.CharField(null=False, max_length=24)
@@ -27,13 +30,31 @@ class Camera(models.Model):
     class Meta:
         db_table = 'camera'
 
+    def __str__(self):
+        return f'{self.name} | ip: {self.ip}'
+
 
 class Label(models.Model):
     camera = models.ForeignKey(Camera, models.CASCADE, blank=True, null=False)
     content = models.CharField(null=False, max_length=100)
     x_axing = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=True)
     y_axing = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=True)
-    
+
     class Meta:
         db_table = 'label'
 
+    def __str__(self):
+        return f'{self.camera}'
+
+
+class MLModel(models.Model):
+    name = models.CharField(null=False, max_length=255)
+    description = models.CharField(null=False, max_length=255)
+    file_path = models.CharField(null=False, max_length=255)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'ml_model'
+
+    def __str__(self):
+        return f'{self.name} | active: {self.active}'
